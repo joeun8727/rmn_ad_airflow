4f16b793256b54f79adb5f19223590109518cbb8
eunjo <joeun8727@naver.com>
2025-06-01 14:47:45 +0900

    탬플릿 테스트 커밋

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,18 @@
+from airflow.sdk import DAG, task
+import datetime
+import pendulum
+
+
+with DAG (
+    dag_id="dags_python_show_templates",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2025, 6, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag :
+    
+    @task(task_id="python_task")
+    def show_templates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+
+    show_templates()
