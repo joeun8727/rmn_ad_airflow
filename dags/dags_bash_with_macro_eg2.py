736886e857634f8a281fcbc8c29d1f4efb952974
eunjo <joeun8727@naver.com>
2025-06-01 15:19:28 +0900

    매크로 테스트 커밋

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -5,16 +5,16 @@ from airflow.providers.standard.operators.bash import BashOperator
 
 with DAG (
     dag_id="dags_bash_with_macro_eg1",
-    schedule="10 0 L * *",
+    schedule="10 0 * * 6#2",
     start_date=pendulum.datetime(2025, 6, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag :
     # START_DATE : 전월 말일, END_DATE : 1일 전
-    bash_task_1 = BashOperator(
-        task_id = 'bash_task1',
+    bash_task_2 = BashOperator(
+        task_id = 'bash_task2',
         env = {
-            'START_DATE' : '{{ data_interval_start.in_timezone("Asia/Seoul") }}',
-            'END_DATE' : '{{ data_interval_start.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days-1) }}'
+            'START_DATE' : '{{ data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days-19) }}',
+            'END_DATE' : '{{ data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days-14) }}'
         },
         
         bash_command = 'echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
