3b74115a8e09dc0d8ca4d1ac314bc965a86e4852
joeun8727 <joeun8727@naver.com>
2026-01-02 15:26:14 +0900

    Update dags_email_operator.py

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -11,7 +11,7 @@ with DAG (
 ) as dag :
     send_email_task = EmailOperator(
         task_id='send_email_task',
-        to='joeun8727@naver.com',
+        to='메일주소',
         subject='Airflow 성공메일',
         html_content='Airflow 작업이 완료되었습니다.'
     )
